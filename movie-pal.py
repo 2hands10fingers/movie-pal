b24c8c28621b112356acda6819036df993222706
Tono <antonio.nogueras@yahoo.com>
2018-05-03 21:55:34 -0500

    Update movie-pal.py

--- movie-pal.py
+++ movie-pal.py
@@ -1,18 +1,23 @@
 from bs4 import BeautifulSoup as bs
 from requests import get
 import pprint
+import webbrowser as wb
+pp = pprint.PrettyPrinter()
 
 class Movie(object):
 	"""docstring for Movie"""
-	def __init__(self, the_title, the_ratings, the_year, 
-		     the_boxoffice, in_the_theaters):
-		
+	def __init__(self, the_title, the_ratings, the_year, the_boxoffice, in_the_theaters, the_id):
 		self.the_title = the_title
 		self.the_ratings = the_ratings
 		self.the_year = the_year
 		self.the_boxoffice = the_boxoffice
 		self.in_the_theaters = in_the_theaters
+		self.the_id = the_id
 
+	@property
+	def id(self):
+		return self.the_id
+	
 	@property
 	def in_theaters(self):
 		return self.in_the_theaters
@@ -35,7 +40,7 @@ class Movie(object):
 
 
 
-class omdb(object):
+class mp(object):
 	"""Main object for OMDB search"""
 	
 	url = 'http://www.omdbapi.com/'
@@ -44,6 +49,7 @@ class omdb(object):
 	def __init__(self):
 		pass
 
+	
 	def in_theaters():
 		source = get('http://www.imdb.com/movies-in-theaters/?ref_=nv_mv_inth_1').text
 		soup = bs(source, 'lxml')
@@ -52,7 +58,7 @@ class omdb(object):
 		return titles
 
 	def requester():
-		rqst = get(omdb.url, params=omdb.parameters).json()
+		rqst = get(mp.url, params=mp.parameters).json()
 		json_wrap = []
 		json_wrap.append(rqst)
 		movie = json_wrap[0]
@@ -60,11 +66,107 @@ class omdb(object):
 		ratings = movie['Ratings']
 		year = movie['Year']
 		box_office = movie["BoxOffice"]
-		in_theaters = omdb.in_theaters()
-		
+		in_theaters = mp.in_theaters()
+		the_id = movie["imdbID"]
 		return Movie(title, ratings, year, 
-			     box_office, in_theaters )
+					box_office, in_theaters, the_id)
 
-	def search_title(self):
-		omdb.parameters['t'] = self
-		return omdb.requester()
+
+
+	def search_title(self, info=False):
+		# try:
+		mp.parameters['t'] = self
+		if info == True:
+			movie = mp.requester().__dict__
+			title, ratings, year, boxoffice, in_theaters, imdID = (
+				movie["the_title"], movie["the_ratings"],
+				movie["the_year"], movie["the_boxoffice"], 
+				movie["in_the_theaters"], movie["the_id"])
+			
+			print('-'*60)
+			print(f"{' '.join(title)}\n\nIMDBid: {imdID}\nBoxOffice: {boxoffice}\nYear: {year}\nRatings: ")
+			for i in ratings:
+				try:
+					print(f'\t\t{i["Source"]} : {i["Value"]}')
+				except KeyError:
+					continue
+			print('-'*60)
+
+		else:
+			return mp.requester()
+
+	def search_id(self):
+		mp.parameters['i'] = self
+		return mp.requester()
+
+	def search(self, key=""):
+		mp.parameters['s'] = self
+		rqst = get(mp.url, params=mp.parameters)
+		movie = rqst.json()["Search"]
+		
+		
+		if key == "":
+			return movie
+		items = []
+		
+		for i in movie:
+			title, year, poster, the_id = i['Title'], i['Year'], i['Poster'], i["imdbID"]
+			details = [title, year, poster, the_id]
+			key = key.lower()
+			
+			if key == 'poster':
+				items.append(poster)
+			if key == 'title':
+				items.append(title)
+			if key == 'imdbid':
+				items.append(the_id)
+			if key == 'year':
+				items.append(year)
+		
+		if not items:
+			raise SystemExit(f'"{key}" is not an available key.\nTry: "Poster", "Title", "imdbID", or "Year"\n')
+		return items
+
+	def show(self, key=""):
+		for i in self:
+			try:
+				movie = mp.search_title(i)
+				title, year, ratings, boxoffice, = (
+					movie.title, movie.year, 
+					movie.ratings, movie.boxoffice)
+			except KeyError:
+				pass
+			finally:
+				key = key.lower()			
+				if key == "":
+					print(i)
+				if key == "title":
+					print(title)
+				if key == "year":
+					print(f'{title} : {year}')
+				if key == "boxoffice":
+					print(f'{title} : {boxoffice}')
+				if key == "ratings":
+					print('-'*60)
+					print(f'\n{title}\n')
+					for i in ratings:
+						try:
+							print(f'\t{i["Source"]} : {i["Value"]}')
+						except KeyError:
+							print("None")
+							continue
+					print('-'*60)
+				if key == "all":
+					print('-'*60)
+					print(f'\n\t{" ".join(title)}\n')
+					print(f'\tBox office: {boxoffice}')
+					print(f'\tYear: {year}')
+					print('\tRatings:')
+					for i in ratings:
+						try:
+							
+							print(f'\t\t{i["Source"]} : {i["Value"]}')
+						except KeyError:
+							print("s")
+							continue
+					print('-'*60)
