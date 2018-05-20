b981584eb2aaa5cda2315d0b11e06967a9560dd6
Tono <antonio.nogueras@yahoo.com>
2018-05-20 01:25:46 -0500

    Create copydata.js

---
+++ js/copydata.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', function() {
+const $ = jQuery;
+
+	function log(string){ console.log(string); }
+
+	function copier(string) {
+		var textArea = document.createElement("textarea");
+		textArea.value = string;
+		document.body.appendChild(textArea);
+		textArea.select();
+		document.execCommand("Copy");
+		textArea.remove();
+	}
+
+	function copyText() {
+		$('.copy-all').addClass('show');
+		setTimeout(function(){$('.copy-all').removeClass('show')}, 3000)
+	}
+
+	$('.link--wrapper').click(function(){
+		let theData = $('.datarow', this);
+		
+		function attri(str){return theData.data(str);}
+		let row = [
+				 attri('title'),
+				 attri('plot'),
+				 attri('boxoffice'),
+				 attri('genre'),
+				 attri('runtime'),
+				 attri('mtc'),
+				 attri('imdb'),
+				 attri('rotten')
+							]
+		let arrangeRow = [row];
+		let processRow = SheetClip.stringify(arrangeRow);
+		
+		copier(processRow);
+		copyText();
+
+	});
+
+	$('.copy-all').click(function(){
+		let classFind = document.getElementsByClassName('datarow')
+		let arrangeRow = [];
+
+		for (var i = classFind.length - 1; i >= 0; i--) {
+			movie = classFind[i]["dataset"];
+			row = [	
+					movie["title"],
+					movie["plot"],
+					movie["boxoffice"],
+					movie["genre"],
+					movie["runtime"],
+					movie["mtc"],
+					movie["imdb"],
+					movie["rotten"]
+				  ];
+			
+			arrangeRow.push(row);
+
+		let processRow = SheetClip.stringify(arrangeRow);
+		
+		copier(processRow)
+		copyText();
+
+		}
+	});
+
+}); //end
