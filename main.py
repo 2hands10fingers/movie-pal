57a3c3eb3aaa168649642d6f7dd7c66d35603778
Tono <antonio.nogueras@yahoo.com>
2018-05-21 23:53:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ parser.add_argument("-id", "--search_id", type=str,
                     help="Search the API by IMDB ID")
 parser.add_argument("-ui", "--generate", action="store_true",
                     help="Search the API by IMDB ID")
+
+parser.add_argument("-rtscore", "--rotten_score", type=str,
+                    help="Tries to scrapes a Rotten Tomatoes page for a given title")
+parser.add_argument("-rtn", '--rotten_search', type=str,
+                    help="Search Rotten Tomatoes to see available titles")
+
 args = parser.parse_args()
 
 
@@ -39,6 +45,11 @@ def main():
             print(movie)
     if args.generate:
         import htmlgen
+    if args.rotten_score:
+        mp.sorter(mp.rotten_scraper(args.rotten_score, the_year=args.key))
+    if args.rotten_search:
+        print(mp.rotten_search(args.rotten_search))
+
 
 if __name__ == '__main__':
     main()
